3ca2d40bc9325d54698c771f56f3ea6b0863d9f4
davlatbekzoirov <davlatbekzoirov@gmail.com>
2026-06-17 20:47:12 +0500

    send email

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('', include('budget.urls')),
     path('roomieratio/', include('roomieratio.urls')),
     path('housing/', include('housing.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
